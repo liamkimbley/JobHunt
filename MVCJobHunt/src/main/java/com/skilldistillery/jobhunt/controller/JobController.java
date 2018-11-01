fafb406650e1336b7e4ccef4697e3e124e6862d3
liamkimbley <liam.kimbley@gmail.com>
2018-11-01 11:44:28 -0700

    project updated

--- MVCJobHunt/src/main/java/com/skilldistillery/jobhunt/controller/JobController.java
+++ MVCJobHunt/src/main/java/com/skilldistillery/jobhunt/controller/JobController.java
@@ -1,11 +1,13 @@
 package com.skilldistillery.jobhunt.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.skilldistillery.jobhunt.entities.JobHuntDAO;
 
+@Controller
 public class JobController {
 
 	@Autowired
